343e5da2a6d44e1202193d6038fe43de5e6bae0d
polemarch <polemarch@192.168.1.159>
2013-10-07 03:29:19 +0300

    begin core project

--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <?php
 
-//version check
-//bootstrap login 
-//php configuration special
-//
-//folder permission check
-//core/modlul/class/function/method
+define('START', microtime(true));
+
+require_once '_apps/boot.inc';
 
 
 
 
-header('Content-type: text/html');
-
-@ini_set("display_errors", 1);
-@ini_set( 'error_reporting', E_ALL ); 
-date_default_timezone_set('Europe/Istanbul');
-$q = isset($_GET ['q']) ? $_GET ['q'] : null;
-$q = rtrim($q, '/');
-$q = explode('/', $q);
-//phpinfo();
 
 
-///
 
-///
+//render time 
+define('END', microtime(true));
+echo '<!--';
+echo END - START;
+echo '-->';
 ?>
