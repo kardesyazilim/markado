21f42fe20311fed51d510c4519e1e9927f6c5d10
polemarch <polemarch@192.168.1.159>
2013-10-07 15:38:54 +0300

    begin core project

---
+++ skins/frontend/default/js/main.js
@@ -0,0 +1,10 @@
+/*
+ * carina erp v.1.03b
+ * kardesyazilim.net istanbulmobile.net
+ * Kardeş Yazılım ve Bilişim Hizmetleri
+ * labs@kardesyazilim.net
+ * labs.kardesyazilim.net
+ * Oct 7, 2013 - 3:37:46 PM  / file name: [main] (main.js)
+ * UTF-8 - markado - default
+ * help doc : main - (main.js)
+ */
