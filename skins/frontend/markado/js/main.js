ee6a3e4420729138ec78a0e40a98fac1e5250eeb
polemarch <polemarch@192.168.1.159>
2013-10-07 15:36:01 +0300

    begin core project

---
+++ skins/frontend/markado/js/main.js
@@ -0,0 +1,10 @@
+/*
+ * carina erp v.1.03b
+ * kardesyazilim.net istanbulmobile.net
+ * Kardeş Yazılım ve Bilişim Hizmetleri
+ * labs@kardesyazilim.net
+ * labs.kardesyazilim.net
+ * Oct 7, 2013 - 5:28:03 AM  / file name: [main] (main.js)
+ * UTF-8 - markado - default
+ * help doc : main - (main.js)
+ */
