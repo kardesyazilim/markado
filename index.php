3aea77f770ca29fd9ae49b04fafaaee5772c7e8d
polemarch <polemarch@192.168.1.159>
2013-10-17 20:59:11 +0300

    markado frontend

--- index.php
+++ index.php
@@ -6,55 +6,6 @@ define('START', microtime(true));
 
 //var_dump($_SESSION);
 //phpinfo();
-set_include_path(get_include_path() . PATH_SEPARATOR . '/Volumes/Golas/backup/jobs/markado/google-api-php-client/src/');
-session_start();
-require_once 'Google_Client.php';
 
-$client = new Google_Client();
 
-$client->setApplicationName('labs.kardesyazilim.net');
-$client->setScopes("http://www.google.com/m8/feeds/");
-// Documentation: http://code.google.com/apis/gdata/docs/2.0/basics.html
-// Visit https://code.google.com/apis/console?api=contacts to generate your
-// oauth2_client_id, oauth2_client_secret, and register your oauth2_redirect_uri.
-// $client->setClientId('insert_your_oauth2_client_id');
-// $client->setClientSecret('insert_your_oauth2_client_secret');
-// $client->setRedirectUri('insert_your_redirect_uri');
-// $client->setDeveloperKey('insert_your_developer_key');
 
-if (isset($_GET['code'])) {
-  $client->authenticate();
-  $_SESSION['token'] = $client->getAccessToken();
-  $redirect = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-  header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-}
-
-if (isset($_SESSION['token'])) {
- $client->setAccessToken($_SESSION['token']);
-}
-
-if (isset($_REQUEST['logout'])) {
-  unset($_SESSION['token']);
-  $client->revokeToken();
-}
-
-if ($client->getAccessToken()) {
-  $req = new Google_HttpRequest("https://www.google.com/m8/feeds/contacts/default/full");
-  $val = $client->getIo()->authenticatedRequest($req);
-
-  // The contacts api only returns XML responses
-$hh = file_get_contents($val->getResponseBody());
-  $response = json_encode(simplexml_load_string($hh));
-  print "<pre>" . print_r(json_decode($response, true), true) . "</pre>";
-
-  // The access token may have been updated lazily.
-  $_SESSION['token'] = $client->getAccessToken();
-} else {
-  $auth = $client->createAuthUrl();
-}
-
-if (isset($auth)) {
-    print "<a class=login href='$auth'>Connect Me!</a>";
-  } else {
-    print "<a class=logout href='?logout'>Logout</a>";
-}
--- skins/frontend/markado/js/main.js
+++ skins/frontend/markado/js/main.js
@@ -8,3 +8,19 @@
  * UTF-8 - markado - default
  * help doc : main - (main.js)
  */
+
+ $(document).ready(function() {
+ 
+ 
+ $('.carousel').carousel({
+  interval: 3000
+})
+ 
+ 
+     $("[rel=broqr]").popover({
+      placement : 'left', //placement of the popover. also can use top, bottom, left or right
+
+      html: 'true', //needed to show html of course
+      content : '<div id="popOverBox"><img src="qrcode.png" width="150" height="150" /></div>' //this is the content of the html box. add the image here or anything you want really.
+});
+});
