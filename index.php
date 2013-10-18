bee772ee927e8252f6dbc9582407ddf3a3755280
polemarch <polemarch@192.168.1.159>
2013-10-18 21:39:00 +0300

    markado frontend

--- index.php
+++ index.php
@@ -6,6 +6,13 @@ define('START', microtime(true));
 
 //var_dump($_SESSION);
 //phpinfo();
+$img_source = "favicon.ico"; // image path/name
+$img_binary = fread(fopen($img_source, "r"), filesize($img_source));
+$img_string = base64_encode($img_binary);
 
+header("Content-type: image/ico");
+echo base64_decode($img_string); 
+
+echo  '<img src="data:image/ico;base64,'.$img_string.'">';
 
 
