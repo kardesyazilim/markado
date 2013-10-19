89aec7a59a964989030af171938968cd2556004f
polemarch <polemarch@192.168.1.159>
2013-10-19 05:09:25 +0300

    carina server install

--- index.php
+++ index.php
@@ -2,17 +2,30 @@
 
 /* session_destroy();
   ob_flush(); */
-define('START', microtime(true));
+//define('START', microtime(true));
+//
+////var_dump($_SESSION);
+////phpinfo();
+//$img_source = "favicon.ico"; // image path/name
+//$img_binary = fread(fopen($img_source, "r"), filesize($img_source));
+//$img_string = base64_encode($img_binary);
+//
+//header("Content-type: image/ico");
+//echo base64_decode($img_string); 
+//
+//echo  '<img src="data:image/ico;base64,'.$img_string.'">';
 
-//var_dump($_SESSION);
-//phpinfo();
-$img_source = "favicon.ico"; // image path/name
-$img_binary = fread(fopen($img_source, "r"), filesize($img_source));
-$img_string = base64_encode($img_binary);
-
-header("Content-type: image/ico");
-echo base64_decode($img_string); 
-
-echo  '<img src="data:image/ico;base64,'.$img_string.'">';
-
-
+phpinfo();
+class Calculator
+{
+    /**
+     * @assert (0, 0) == 0
+     * @assert (0, 1) == 1
+     * @assert (1, 0) == 1
+     * @assert (1, 1) == 2
+     */
+    public function add($a, $b)
+    {
+        return $a + $b;
+    }
+}
